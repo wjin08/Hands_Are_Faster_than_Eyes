37f40c159434f27bc8df35f373171ab6e5ea337a
YunChiyeong <cldud97@naver.com>
2025-09-24 14:25:41 +0900

    값 매핑

--- src/opencv_ws/palm_center_show_cuda_clinet.cpp
+++ src/opencv_ws/palm_center_show_cuda_clinet.cpp
@@ -7,211 +7,109 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <pthread.h>
-#include <signal.h>
-#include <sys/select.h>
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/cuda.hpp>      // ← 추가
+#include <opencv2/core/cuda.hpp>
 #include <iostream>
+#include <algorithm> // std::clamp
 
 #define BUF_SIZE  1025
 #define NAME_SIZE 20
-#define ARR_CNT 5
 
 using namespace cv;
 using namespace std;
 
-void* send_msg(void* arg);
-void* recv_msg(void* arg);
-void* opencv_coordinate(void* arg);
-void error_handling(const char* msg);
+static void error_handling(const char* msg) {
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
 
-char name[NAME_SIZE]="[Default]";
-char msg[BUF_SIZE];
+char namebuf[NAME_SIZE] = "[Default]";
+char msgbuf[BUF_SIZE];
 
 int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in serv_addr;
-	pthread_t snd_thread, rcv_thread, opencv_thread;
-	void * thread_return;
+    if (argc != 4) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+        return 1;
+    }
 
-	if(argc != 4) {
-		printf("Usage : %s <IP> <port> <name>\n",argv[0]);
-		exit(1);
-	}
+    snprintf(namebuf, sizeof(namebuf), "%s", argv[3]);
 
-	sprintf(name, "%s",argv[3]);
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) error_handling("socket() error");
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
-		error_handling("socket() error");
+    sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
 
-	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
-		error_handling("connect() error");
+    // 로그인/패스워드 패킷 전송
+    int passlen = snprintf(msgbuf, sizeof(msgbuf), "[%s:PASSWD]", namebuf);
+    if (passlen > 0) (void)write(sock, msgbuf, (size_t)passlen);
 
-	sprintf(msg,"[%s:PASSWD]",name);
-	(void)write(sock, msg, strlen(msg));
-	pthread_create(&rcv_thread, NULL, recv_msg, (void *)&sock);
-	pthread_create(&snd_thread, NULL, send_msg, (void *)&sock);
-    pthread_create(&opencv_thread, NULL, opencv_coordinate, (void *)&sock);
-
-	pthread_join(snd_thread, &thread_return);
-    pthread_join(opencv_thread, &thread_return); 
-	//	pthread_join(rcv_thread, &thread_return);
-
-	close(sock);
-	return 0;
-}
-
-void * send_msg(void * arg)
-{
-	int *sock = (int *)arg;
-	int str_len;
-	int ret;
-	fd_set initset, newset;
-	struct timeval tv;
-	char name_msg[NAME_SIZE + BUF_SIZE+2];
-
-	FD_ZERO(&initset);
-	FD_SET(STDIN_FILENO, &initset);
-
-	fputs("Input a message! [ID]msg (Default ID:ALLMSG)\n",stdout);
-	while(1) {
-		memset(msg,0,sizeof(msg));
-		name_msg[0] = '\0';
-		tv.tv_sec = 1;
-		tv.tv_usec = 0;
-		newset = initset;
-		ret = select(STDIN_FILENO + 1, &newset, NULL, NULL, &tv);
-		if(FD_ISSET(STDIN_FILENO, &newset))
-		{
-			fgets(msg, BUF_SIZE, stdin);
-			if(!strncmp(msg,"quit\n",5)) {
-				*sock = -1;
-				return NULL;
-			}
-			else if(msg[0] != '[')
-			{
-				strcat(name_msg,"[ALLMSG]");
-				strcat(name_msg,msg);
-			}
-			else
-				strcpy(name_msg,msg);
-			if(write(*sock, name_msg, strlen(name_msg))<=0)
-			{
-				*sock = -1;
-				return NULL;
-			}
-		}
-		if(ret == 0)
-		{
-			if(*sock == -1)
-				return NULL;
-		}
-	}
-}
-
-void * recv_msg(void * arg)
-{
-	int * sock = (int *)arg;
-	int i;
-	char *pToken;
-	char *pArray[ARR_CNT]={0};
-
-	char name_msg[NAME_SIZE + BUF_SIZE +1];
-	int str_len;
-	while(1) {
-		memset(name_msg,0x0,sizeof(name_msg));
-		str_len = read(*sock, name_msg, NAME_SIZE + BUF_SIZE );
-		if(str_len <= 0)
-		{
-			*sock = -1;
-			return NULL;
-		}
-		name_msg[str_len] = 0;
-		fputs(name_msg, stdout);
-
-		/*   	pToken = strtok(name_msg,"[:]");
-			i = 0;
-			while(pToken != NULL)
-			{
-			pArray[i] =  pToken;
-			if(i++ >= ARR_CNT)
-			break;
-			pToken = strtok(NULL,"[:]");
-			}
-
-		//		printf("id:%s, msg:%s,%s,%s,%s\n",pArray[0],pArray[1],pArray[2],pArray[3],pArray[4]);
-		printf("id:%s, msg:%s\n",pArray[0],pArray[1]);
-		*/
-	}
-}
-void* opencv_coordinate(void* arg)
-{
-	int* sock = (int*) arg;
+    // CUDA 정보 출력(옵션)
     cout << "Have CUDA : " << cv::cuda::getCudaEnabledDeviceCount() << endl;
     if (cv::cuda::getCudaEnabledDeviceCount() > 0) {
         cv::cuda::printShortCudaDeviceInfo(cv::cuda::getDevice());
     }
+
     VideoCapture cap(0);
-    if (!cap.isOpened()) { cerr << "cam open fail\n"; return nullptr; }
+    if (!cap.isOpened()) { cerr << "cam open fail\n"; close(sock); return 1; }
 
     const Scalar YCrCb_low(0,133,77), YCrCb_high(255,173,127);
-    Mat frame,ycrcb,mask,morph;
+    Mat frame, ycrcb, mask, morph;
 
     while (true) {
         if (!cap.read(frame) || frame.empty()) break;
 
-        // GPU 전처리 사용하려면 cudaimgproc 헤더/링크도 필요
-        // (지금은 CPU 경로 그대로 유지)
         cvtColor(frame, ycrcb, COLOR_BGR2YCrCb);
         inRange(ycrcb, YCrCb_low, YCrCb_high, mask);
         Mat k = getStructuringElement(MORPH_ELLIPSE, Size(5,5));
         morphologyEx(mask, morph, MORPH_OPEN, k);
         morphologyEx(morph, morph, MORPH_CLOSE, k, Point(-1,-1), 2);
 
-        // 중심 계산(이전 코드와 동일)
         vector<vector<Point>> contours;
         findContours(morph.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
         if (!contours.empty()) {
-            size_t mi=0; double ma=0;
-            for (size_t i=0;i<contours.size();++i){ double a=contourArea(contours[i]); if(a>ma){ma=a; mi=i;} }
-            if (ma>1000) {
+            size_t mi = 0; double ma = 0;
+            for (size_t i=0;i<contours.size();++i){
+                double a = contourArea(contours[i]);
+                if (a > ma) { ma = a; mi = i; }
+            }
+            if (ma > 1000) {
                 Mat hand = Mat::zeros(morph.size(), CV_8U);
                 drawContours(hand, contours, (int)mi, Scalar(255), FILLED);
                 Mat dist; distanceTransform(hand, dist, DIST_L2, 5);
                 double maxVal; Point center;
                 minMaxLoc(dist, nullptr, &maxVal, nullptr, &center);
                 circle(frame, center, 8, Scalar(0,255,255), FILLED);
-                cout << "center: " << center.x << ", " << center.y << endl;
 
-                char buf[32];
-                int n = snprintf(buf, sizeof(buf), "%d\n", center.x); // 개행은 라인 구분용
+                // 0~640 → 0~255 스케일링
+                int scaled = (center.x * 255) / 640;
+                int val = std::clamp(scaled, 0, 255);
+
+                char txbuf[64];
+                int n = snprintf(txbuf, sizeof(txbuf), "[KSH_QT]LED@0x%02x\n", val);
+
                 if (n > 0) {
-                    if (write(*sock, buf, (size_t)n) <= 0) {  // 에러 시 종료
-                        *sock = -1;
+                    std::cout << "TX: " << txbuf; // 로그
+                    if (write(sock, txbuf, (size_t)n) <= 0) {
+                        cerr << "write() error\n";
                         break;
-						return nullptr;
                     }
                 }
             }
         }
+
         imshow("camera", frame);
         if ((waitKey(1) & 0xFF) == 'q') break;
-        if (*sock == -1) break;
     }
-    return nullptr;
-}
-void error_handling(const char * msg)
-{
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
+
+    close(sock);
+    return 0;
 }
